f1b368ceb7e28531a37e5b6a270773d25dde5722
Iain Emsley <iain.emsley@kellogg.ox.ac.uk>
2016-10-17 16:43:17 +0100

    testing the fluent interface

--- src/lom/testipmi.py
+++ src/lom/testipmi.py
@@ -1,23 +1,33 @@
-from nose.tools import assert_true, assert_false
+from nose.tools import assert_true, assert_false, eq_
 
 from ipmi import lom_ipmi
 
 def test_arg_power_options():
     ipmi = lom_ipmi()
-    assert_true(ipmi.check_power("on"),True)
+    assert_true(ipmi._check_power("on"),True)
 
 def test_false_arg_power_options():
     ipmi = lom_ipmi()
-    assert_false(ipmi.check_power("restart"),True)
+    assert_false(ipmi._check_power("restart"),True)
 
 def test_arg_ip_exists():
     ipmi = lom_ipmi()
-    assert_true(ipmi.check_ip("5"),True)
+    assert_true(ipmi._check_ip("5"),True)
 
 def test_arg_ip_does_not_exist():
     ipmi = lom_ipmi()
-    assert_false(ipmi.check_ip(""),True)
+    assert_false(ipmi._check_ip(""),True)
 
 def test_arg_ip_not_too_long():
     ipmi = lom_ipmi()
-    assert_false(ipmi.check_ip("12345"),True)
+    assert_false(ipmi._check_ip("12345"),True)
+
+def test_auth():
+    ipmi =lom_ipmi().connection_auth("user", "pass")
+    eq_(ipmi.user, "user")
+    eq_(ipmi.passwd, "pass")
+
+def test_build_command():
+    ipmi =lom_ipmi().connection_auth("user", "pass").host("134").command("on")
+    cmd_to_string = ' '.join(ipmi.build_command())
+    eq_("ipmitools -v -I lanplus -H 134 -U user -P pass chassis power on", cmd_to_string)
